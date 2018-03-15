62f207792b30829c67aa77d49e132834696c2540
Paulo Otavio Gomes de Abreu Pinto <paulinho.speed@hotmail.com>
2018-03-15 19:29:57 -0300

    add parse and form

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,24 @@
 import { Component } from '@angular/core';
+import {Parse } from 'parse'
+import {NgForm} from '@angular/forms';
+
+var GameScore = Parse.Object.extend("User");
+var query = new Parse.Query(GameScore);
+query.find({
+  success: function(results) {
+    alert("Successfully retrieved " + results.length + " scores.");
+    // Do something with the returned Parse.Object values
+    console.log(results);
+    for (var i = 0; i < results.length; i++) {
+      var object = results[i];
+
+      alert(object.id + ' - ' + object.get('playerName'));
+    }
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
 
 @Component({
   selector: 'app-root',
@@ -7,4 +27,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+ 
+  onSubmit(f: NgForm) {
+    //cadastrar user
+
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
+
+
 }
